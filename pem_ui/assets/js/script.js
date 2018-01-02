e7494dcd39becaae214e0a566f0158f28b99b341
ipcrm <mcadorette@gmail.com>
2018-01-02 08:23:22 -0500

    adding the compare env links, updates

--- pem_ui/assets/js/script.js
+++ pem_ui/assets/js/script.js
@@ -30,6 +30,11 @@ pemApp.config(function($routeProvider) {
             controller  : 'env_compareController'
         })
 
+        .when('/env_compare_prep/:env1', {
+            templateUrl : 'assets/pages/env_compare_prep.html',
+            controller  : 'env_compare_prepController'
+        })
+
 
         .otherwise({redirectTo: '/environments'});
 
@@ -62,10 +67,27 @@ pemApp.controller('env_compareController', function($scope, $http, $routeParams)
     $http.get(conn_string + '/envs/compare/' + $routeParams.env1 + "/" + $routeParams.env2)
       .then(function(response){
         console.log(response.data);
+        console.log(response.data[Object.keys(response.data)[0]]);
         $scope.env1 = $routeParams.env1;
         $scope.env2 = $routeParams.env2;
         $scope.envdata = response.data;
     });
+});
+
+pemApp.controller('env_compare_prepController', function($scope, $http, $routeParams, $location) {
+    $http.get(conn_string + '/envs')
+      .then(function(response){
+        var envs = response.data;
+        delete envs[$routeParams.env1];
+
+        $scope.env1 = $routeParams.env1;
+        $scope.envdata = envs;
+    });
+
+    $scope.redirect = function() {
+        $location.path('/env_compare/' + $scope.env1 + "/" + $scope.selectedItem);
+    }
+
 });
 
 pemApp.directive('backButton', function(){
