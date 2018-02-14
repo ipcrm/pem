a22647065c3612367daa2c5199f9d1540d8cbe2e
Thomas Linkin <trlinkin@gmail.com>
2018-02-13 16:01:04 -0800

    Finally fix the versions form

--- pem_ui/assets/js/script.js
+++ pem_ui/assets/js/script.js
@@ -76,7 +76,7 @@ pemApp.controller('mod_detailController', function($scope, $http, $routeParams) 
 
 pemApp.controller('mod_createController', function($scope, $http) {
 
-  $scope.selected_versionss = [];
+  $scope.selected_versions = [];
 
   $scope.search_for_mod = function(search_string) {
     $scope.loading = true;
